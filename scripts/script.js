8e20eafc2e380ae876fac50412e5d2c11bffd45c
airu020604 <airu020604@gmail.com>
2026-07-28 05:50:18 +0900

    Initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -147,7 +147,7 @@ isParticlePlaying = true;
   await mindarThree.start();
 document.getElementById("cameraUI").style.position = "fixed";
 document.getElementById("cameraUI").style.left = "50%";
-document.getElementById("cameraUI").style.bottom = "30px";
+document.getElementById("cameraUI").style.bottom = "300px";
 document.getElementById("cameraUI").style.transform = "translateX(-50%)";
 
   animate(renderer, scene, camera);
