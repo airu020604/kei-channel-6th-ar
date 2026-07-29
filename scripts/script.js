8485c7990542afdf4e261443b500156d7fe1f0b3
airu020604 <airu020604@gmail.com>
2026-07-29 11:26:56 +0900

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -20,10 +20,22 @@ let mode = "photo";
 let currentState;
 let idleBaseY = -0.6;
 let blinkTimer = 0;
-
 let mixer = null;
 let animationAction = null;
+let modelRoot = null;
+let worldLocked = false;
+let isDragging = false;
+let lastTouchX = 0;
+let rotateRoot = null;
 
+let previousMouseX = 0;
+let rotationY = 0;
+let previousTouchX = 0;
+
+
+const lockedPosition = new THREE.Vector3();
+const lockedQuaternion = new THREE.Quaternion();
+const lockedScale = new THREE.Vector3();
 
 
 // ===== 変化しない変数の設定 =====
@@ -44,13 +56,12 @@ const Motion = {
 //playMotion(Motion.DANCE);
 
 currentState = State.HIDDEN;
-
 const photoBtn = document.querySelector("#photoBtn");
 const effectBtn = document.querySelector("#effectBtn");
 const startBtn = document.querySelector("#startBtn");
 
 
-//スタート
+// ===== スタート関数 Start =====
 const start = async () => {
   
   const mindarThree = new MindARThree({
@@ -58,87 +69,105 @@ const start = async () => {
     imageTargetSrc: "./targets/targets.mind"
   });
 
-
   const renderData = mindarThree;
   renderer = renderData.renderer;
-renderer.outputColorSpace = THREE.SRGBColorSpace;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1.0;
+  renderer.outputColorSpace = THREE.SRGBColorSpace;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1.0;
   const scene = renderData.scene;
   const camera = renderData.camera;
 
   renderer.preserveDrawingBuffer = true;
+  renderer.domElement.style.pointerEvents = "none";
 
   
-  // ===== ライト設定関数 =====
+  // ===== ライト設定関数 呼び出し =====
   createLight(scene);
 
   // ===== マーカー0番 =====
   const anchor = mindarThree.addAnchor(0);
 
 
+//
   anchor.onTargetFound = () => {
-  vrm.scene.visible = true;
-  particleStartTime = clock.getElapsedTime();
+    
+    vrm.scene.visible = true;
+    //particleStartTime = clock.getElapsedTime();
 
-  if (!vrm) return;
+    if (!vrm) return;
+
     if (isTracking) return;
-      isTracking = true;
-      appearTime = 0;
-      vrm.scene.visible = true;
 
-      vrm.scene.position.set(0, -0.8, 0);
-      vrm.scene.scale.set(0.6, 0.6, 0.6);
-      vrm.lookAt = null;
+    isTracking = true;
+    appearTime = 0;
+    vrm.scene.visible = true;
+    vrm.scene.position.set(0, -0.8, 0);
+    vrm.scene.scale.set(1, 1, 1);
+    vrm.lookAt = null;
+    vrm.scene.traverse((obj) => {
 
-      vrm.scene.traverse((obj) => {
+      if (!obj.isMesh) return;
 
-        if (!obj.isMesh) return;
-        const name = obj.name.toLowerCase();
+      const name = obj.name.toLowerCase();
 
-    // 目・顔は透明化しない
-       if (
+      // 目・顔は透明化しない
+      if (
         name.includes("eye") ||
         name.includes("face")
-       ) {
-        return;
+        ) {
+          return;
+        }
+
+      if (animationAction) {
+        animationAction.reset();
+        animationAction.play();
       }
-
-    obj.material.transparent = true;
-    //obj.material.opacity = 0;
-
+    });
     if (animationAction) {
+      animationAction.reset();
+      animationAction.play();
+    }
 
-    animationAction.reset();
+  /*  
+  if (!worldLocked) {
+    modelRoot.updateMatrixWorld(true);
+    modelRoot.getWorldPosition(lockedPosition);
+    modelRoot.getWorldQuaternion(lockedQuaternion);
+    modelRoot.getWorldScale(lockedScale);
+    anchor.group.remove(modelRoot);
+    scene.add(modelRoot);
+    modelRoot.position.copy(lockedPosition);
+    modelRoot.quaternion.copy(lockedQuaternion);
+    modelRoot.scale.copy(lockedScale);
+    worldLocked = true;
+  }*/
+};
 
-    animationAction.play();
-
-}
-
-});
-
-
-
-  };
-
+  // ===== ターゲットを見失った時 =====
   anchor.onTargetLost = () => {
-  vrm.scene.visible = false;
+    if (!worldLocked) {
+      vrm.scene.visible = false;
+    }
   };
-
-
 
   // ===== Cube作成関数 =====
   const cube = createCube();
 
-  // ===== VRM読み込み関数 =====
+  // ===== VRM読み込み関数 呼び出し =====
   await loadVRM(anchor);
 
+  // ===== VRMA読み込み関数 呼び出し =====
   await loadVRMA();
   
   //anchor.group.add(cube);
   await mindarThree.start();
+  setupInput();
   animate(renderer, scene, camera);
+
+
+  
 };
+// ===== スタート関数 End =====
 
 // ===== Cube作成関数 =====
 function createCube() {
@@ -149,57 +178,56 @@ function createCube() {
   return cube;
 }
 
-// ===== ライト設定関数 =====
+// ===== ライト設定関数 Start =====
 function createLight(scene){
+  const hemi = new THREE.HemisphereLight(
+    0xffffff,
+    0x666666,
+    2
+  );
+  scene.add(hemi);
 
-    const hemi = new THREE.HemisphereLight(
-        0xffffff,
-        0x666666,
-        2
-    );
-
-    scene.add(hemi);
-
-    const dir = new THREE.DirectionalLight(
-        0xffffff,
-        3
-    );
-
-    dir.position.set(0,3,2);
-
-    scene.add(dir);
-
+  const dir = new THREE.DirectionalLight(
+    0xffffff,
+    3
+  );
+  dir.position.set(0,3,2);
+  scene.add(dir);
 }
+// ===== ライト設定関数 End =====
 
 
-// ===== VRM読み込み関数 =====
+// ===== VRM読み込み関数 Start =====
 function loadVRM(anchor) {
   return new Promise((resolve, reject) => {
     const loader = new GLTFLoader();
     loader.register(parser => new VRMLoaderPlugin(parser));
     loader.load("./models/kei.vrm",
-      (gltf) => {
-        vrm = gltf.userData.vrm;
-if (vrm.lookAt) {
-    vrm.lookAt.autoUpdate = false;
-}
+    (gltf) => {
+      vrm = gltf.userData.vrm;
+      if (vrm.lookAt) {
+        vrm.lookAt.autoUpdate = false;
+      }
 
-        vrm.scene.scale.set(1, 1, 1);
-        vrm.scene.position.set(0, -0.6, 0);
-        idleBaseY = -0.6;
-        vrm.scene.rotation.y = Math.PI;
+      vrm.scene.scale.set(1, 1, 1);
+      vrm.scene.position.set(0, -0.6, 0);
+      idleBaseY = -0.6;
+      vrm.scene.rotation.y = Math.PI;
 
-        vrm.scene.traverse((obj) => {
+      vrm.scene.traverse((obj) => {
+        if (obj.material) {
+          obj.material.needsUpdate = true;
+        }
+      });
 
-          if (obj.material) {
+      modelRoot = new THREE.Group();
+      rotateRoot = new THREE.Group();
+      rotateRoot.add(vrm.scene);
+      modelRoot.add(rotateRoot);
+      anchor.group.add(modelRoot);
 
-    obj.material.needsUpdate = true;
 
-}
-        });
-
-        anchor.group.add(vrm.scene);
-        vrm.scene.visible = false;
+        //vrm.scene.visible = false;
         resolve();
       },
       undefined,
@@ -209,107 +237,140 @@ if (vrm.lookAt) {
 }
 
 
-
+// ===== ロードVRMA設定関数 Start =====
 async function loadVRMA() {
+  const loader = new GLTFLoader();
 
-    const loader = new GLTFLoader();
-
-    loader.register((parser) => {
-        return new VRMAnimationLoaderPlugin(parser);
-    });
-
-    const gltf = await loader.loadAsync("./motions/idle.vrma");
-
-    const vrmAnimation = gltf.userData.vrmAnimations[0];
-
-    const clip = createVRMAnimationClip(vrmAnimation, vrm);
-
-    mixer = new THREE.AnimationMixer(vrm.scene);
-
-    animationAction = mixer.clipAction(clip);
-    animationAction.reset();
-
-animationAction.setLoop(THREE.LoopOnce, 1);
-
-animationAction.clampWhenFinished = true;
-
-animationAction.enabled = true;
-
-animationAction.play();
-
-animationAction.setLoop(
-    THREE.LoopOnce,
-    1
-);
-
-
-    animationAction.play();
-
-}
-
-
-function animate(renderer, scene, camera) {
-
-  renderer.setAnimationLoop(() => {
-
-    const delta = clock.getDelta();
-
-
-
-
-
-
-
-if (mixer) {
-
-    mixer.update(delta);
-
-}
-
-    // VRM更新
-    if (vrm) {
-
-      vrm.update(delta);
-      
-
-if(vrm){
-
-    vrm.update(delta);
-
-}
-
-    }
-
-    renderer.render(scene, camera);
-
+  loader.register((parser) => {
+    return new VRMAnimationLoaderPlugin(parser);
   });
 
-}
+  const gltf = await loader.loadAsync("./motions/idle.vrma");
+  const vrmAnimation = gltf.userData.vrmAnimations[0];
+  const clip = createVRMAnimationClip(vrmAnimation, vrm);
 
+  mixer = new THREE.AnimationMixer(vrm.scene);
+  animationAction = mixer.clipAction(clip);
+  animationAction.reset();
+  animationAction.setLoop(THREE.LoopOnce, 1);
+  animationAction.clampWhenFinished = true;
+  animationAction.enabled = true;
+  animationAction.setLoop(
+    THREE.LoopOnce,
+    1
+  );
+  animationAction.play();
+}
+// ===== ロードVRMA設定関数 End =====
+
+
+// ===== アニメーション関数 Start =====
+function animate(renderer, scene, camera) {
+  renderer.setAnimationLoop(() => {
+    const delta = clock.getDelta();
+    if (mixer) {
+      mixer.update(delta);
+    }
+
+    if (rotateRoot) {
+      rotateRoot.rotation.y = rotationY;
+    }
+    // VRM更新
+    if (vrm) {
+      vrm.update(delta);
+    }
+    renderer.render(scene, camera);
+  });
+}
+// ===== アニメーション関数 End =====
 
 
 photoBtn.onclick = async () => {
-
-    mode = "photo";
-
-    document.querySelector("#menu").style.display = "none";
-    document.querySelector("#container").style.display = "block";
-
-    await start();
-
-    captureBtn.style.display = "block";
-
+  mode = "photo";
+  document.querySelector("#menu").style.display = "none";
+  document.querySelector("#container").style.display = "block";
+  await start();
+  captureBtn.style.display = "block";
 };
 
 effectBtn.onclick = async () => {
-
     mode = "effect";
-
     document.querySelector("#menu").style.display = "none";
     document.querySelector("#container").style.display = "block";
-
     await start();
-
     captureBtn.style.display = "none";
-
 };
+
+
+
+function setupInput() {
+
+    const container = document.querySelector("#container");
+
+    container.style.touchAction = "none";
+
+    container.addEventListener("mousedown", (e) => {
+
+        isDragging = true;
+        previousMouseX = e.clientX;
+
+    });
+
+    window.addEventListener("mouseup", () => {
+
+        isDragging = false;
+
+    });
+
+    window.addEventListener("mousemove", (e) => {
+
+        if (!isDragging) return;
+        if (!rotateRoot) return;
+
+        const dx = e.clientX - previousMouseX;
+
+        previousMouseX = e.clientX;
+
+        rotationY += dx * 0.01;
+        console.log("rotation", rotationY);
+
+    });
+
+
+
+container.addEventListener("touchstart", (e) => {
+
+    if (e.touches.length !== 1) return;
+
+    isDragging = true;
+
+    previousTouchX = e.touches[0].clientX;
+
+});
+
+container.addEventListener("touchmove", (e) => {
+
+    if (!isDragging) return;
+    if (!rotateRoot) return;
+    if (e.touches.length !== 1) return;
+
+    const dx = e.touches[0].clientX - previousTouchX;
+
+    previousTouchX = e.touches[0].clientX;
+
+    rotationY += dx * 0.01;
+
+});
+
+
+container.addEventListener("touchend", () => {
+
+    isDragging = false;
+
+});
+
+
+}
+
+
+
